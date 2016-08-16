363b9844642a87d7677bcc0018073587bc975ca4
aleksandra-majchrzak <aleksandra.a.majchrzak@gmail.com>
2016-08-16 23:40:12 +0200

    Created pokemon class.

---
+++ app/src/main/java/com/example/pokeapp/Pokemon.java
@@ -0,0 +1,12 @@
+package com.example.pokeapp;
+
+/**
+ * Created by Mohru on 16.08.2016.
+ */
+public class Pokemon {
+    String name;
+    String type;
+    String height;
+    String weight;
+    String imagePath;
+}
